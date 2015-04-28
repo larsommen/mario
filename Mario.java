f3b8f28c68e90799095441bb0d9055b18d597778
Mikkel Buchvardt <mikkelbuchvardt@Mikkels-MacBook-Air.local>
2015-04-28 10:10:24 +0200

    main added

---
+++ Mario.java
@@ -0,0 +1,20 @@
+  
+public class Mario{
+
+   public static void main(String[] args) {
+
+   		int numberOfStrings = Integer.parseInt(StdIn.readLine());
+   		int lengthOfStrings = Integer.parseInt(StdIn.readLine());
+		
+		String[] a = new String[numberOfStrings];
+   		
+   		int index = 0;
+   		while(StdIn.hasNextLine()){
+   			a[index]= StdIn.readLine();
+   			index++;
+   		}
+        
+        for (int i = 0; i <numberOfStrings ; i++){
+         StdOut.println(a[i]);
+    	}
+}
